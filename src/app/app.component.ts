9bb8e625bd947c790968a19bbf1206f9e79090c3
guywarburg <guy.warburg@gmail.com>
2018-01-16 17:25:55 +0200

    add bg animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   public dataUrl: string;
   public hideFileUploader: boolean = false;
+  public isRevealed: boolean = false;
+  private interval: any;
+
+  ngOnInit() {
+    this.startInterval();
+  }
+
+  startInterval() {
+    this.interval = setInterval(() => {
+      this.isRevealed = !this.isRevealed;
+      console.log('this.isRevealed', this.isRevealed);
+    }, 3000);
+  }
 
   hendleFileUpload(e) {
     this.dataUrl = e;
